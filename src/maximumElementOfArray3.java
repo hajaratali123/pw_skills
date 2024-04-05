4623560e47da88b8e8aa124b4bff7e687f9c43a8
hajaratali <hazarathalibibijan63@gmail.com>
2024-04-05 22:20:08 +0530

    finding maximum of array

---
+++ src/maximumElementOfArray3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class maximumElementOfArray3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of Array:");
+        int s = sc.nextInt();
+        int[] arr = new int[s];
+        System.out.println("Enter the elements of Array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int res=maxNum(arr);
+        System.out.println("The maximum of array is "+res);
+    }
+    @org.jetbrains.annotations.Contract(pure = true)
+    public static int maxNum(int[] arr){
+        int max=arr[0];
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]>max){
+                max=Math.max(max,arr[i]);
+            }
+        }
+
+        return max;
+    }
+}
