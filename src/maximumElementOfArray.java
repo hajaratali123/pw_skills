3e3263ffff2dbe0902f665749d22e2dc5835623a
hajaratali <hazarathalibibijan63@gmail.com>
2024-04-05 22:19:51 +0530

    finding maximum of array

---
+++ src/maximumElementOfArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class maximumElementOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of Array:");
+        int s = sc.nextInt();
+        int[] arr = new int[s];
+        System.out.println("Enter the elements of Array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int res=maxNum(arr);
+        System.out.println(res);
+    }
+    public static int maxNum(int[] arr){
+        int max=arr[0];
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        return max;
+    }
+}
---
+++ src/maximumElementOfArray2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class maximumElementOfArray2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of Array:");
+        int s = sc.nextInt();
+        int[] arr = new int[s];
+        System.out.println("Enter the elements of Array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int res=maxNum(arr);
+        System.out.println(res);
+    }
+    public static int maxNum(int[] arr){
+
+        Integer integer = null;
+        // bt taking the minimum value
+        int max= integer.MIN_VALUE;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        return max;
+    }
+}
