ffe31afafd77245f10cb90ac72b89fd0127955a9
hajaratali <hazarathalibibijan63@gmail.com>
2024-08-09 19:19:47 +0530

    problems on Arrays

--- src/leetcode/Arrays/SearchIn2DMatrix.java
+++ src/leetcode/Arrays/SearchIn2DMatrix.java
@@ -4,19 +4,20 @@ public class SearchIn2DMatrix {
 
 
     public static boolean SearchMatrix(int[][] arr , int target){
+
         int m= arr.length,n = arr[0].length;
         for (int i = 0; i <m ; i++) {
             for (int j = 0; j <n ; j++) {
                 if (arr[i][j] == target) return true;
 
             }
+
         }
         return false;
     }
     public static void main(String[] args) {
         int[][] arr = {{2,3,6,7},{5,7,9,9},{8,4,7,9}};
-        int m= arr.length,n = arr[0].length;
-        int target = 10;
+        int target = 2;
         boolean res =SearchMatrix(arr,target);
         System.out.println(res);
 
---
+++ src/leetcode/Arrays/SearchIn2DMatrixA2.java
@@ -0,0 +1,7 @@
+package leetcode.Arrays;
+
+public class SearchIn2DMatrixA2 {
+    public static void main(String[] args) {
+
+    }
+}
